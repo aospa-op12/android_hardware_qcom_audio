c4b91985c15c6970147e66d18ef6ee27e036d843
Sean Du <do4suki@gmail.com>
2025-02-08 12:06:25 +0800

    Reapply "primary-hal: Update paths to agm and pal"
    This reverts commit 0a01b0a38bcc154b2f7394d7218890bb2efa576b.
    
    Change-Id: I8937209105d21bc25dfd20b7cb1da24240bebc60
    Signed-off-by: Sean Du <do4suki@gmail.com>

--- hal/audio_extn/Android.mk
+++ hal/audio_extn/Android.mk
@@ -43,7 +43,7 @@ LOCAL_SHARED_LIBRARIES := \
     libar-pal
 
 LOCAL_C_INCLUDES := \
-    vendor/qcom/opensource/pal \
+    $(call project-path-for,qcom-audio)/pal \
     $(call project-path-for,qcom-audio)/primary-hal/hal \
     $(call project-path-for,qcom-audio)/primary-hal/hal/audio_extn \
     $(LOCAL_PATH)/inc \
@@ -87,7 +87,7 @@ LOCAL_SHARED_LIBRARIES := \
     libar-pal
 
 LOCAL_C_INCLUDES := \
-    vendor/qcom/opensource/pal \
+    $(call project-path-for,qcom-audio)/pal \
     $(call project-path-for,qcom-audio)/primary-hal/hal \
     $(call project-path-for,qcom-audio)/primary-hal/hal/audio_extn \
     $(LOCAL_PATH)/inc \
